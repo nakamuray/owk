c392b43e111c97e9f4cfa1c52b9ca5ee6158a360
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2012-10-24 17:07:44 +0900

    Owk.Module.importProgram: same as `import_` but for `Program`

--- Owk/Module.hs
+++ Owk/Module.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Owk.Module (import_) where
+module Owk.Module
+    ( import_
+    , importProgram
+    ) where
 
 import Control.Monad.Reader (asks, local)
 
@@ -7,20 +10,23 @@ import qualified Data.Text as T
 
 import Owk.Interpreter
 import Owk.Type
+import Owk.AST (Program)
 import Owk.Parser (parseOwkFile)
 import qualified Owk.Namespace as Namespace
 
 import_ :: FilePath -> Owk Object
 import_ fpath = do
-    g <- asks Namespace.extractGlobal
-    s <- liftIO $ Namespace.create g
     ret <- liftIO $ parseOwkFile fpath
     case ret of
         Left e  -> exception $ String $ T.pack e
-        Right prog -> do
-            local (const s) $ do
-                Namespace.define "__file__" $ String (T.pack fpath)
-                interpret_ prog
-            h <- liftIO $ Namespace.toHash (Namespace.currentNamepace s)
-            return $ Dict h
+        Right prog -> importProgram fpath prog
 
+importProgram :: FilePath -> Program -> Owk Object
+importProgram fpath prog = do
+    g <- asks Namespace.extractGlobal
+    s <- liftIO $ Namespace.create g
+    local (const s) $ do
+        Namespace.define "__file__" $ String (T.pack fpath)
+        interpret_ prog
+    h <- liftIO $ Namespace.toHash (Namespace.currentNamepace s)
+    return $ Dict h
