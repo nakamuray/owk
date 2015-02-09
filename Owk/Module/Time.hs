1a7f5c9ca59e5c9731d915fa54caec0dc5f155d0
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2015-02-09 23:31:25 +0900

    add time.fromPosix function, time.format also accept Number

--- Owk/Module/Time.hs
+++ Owk/Module/Time.hs
@@ -4,6 +4,7 @@ module Owk.Module.Time (time) where
 
 import Data.Time (UTCTime, utcToLocalZonedTime)
 import Data.Time.Format (formatTime, parseTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Data.Typeable (cast)
 import System.Locale (defaultTimeLocale)
 
@@ -19,11 +20,13 @@ time = Dict $ H.fromList
     [ ("format", builtin2M format)
     , ("formatLocal", builtin2M formatLocal)
     , ("parse", builtin2M parse)
+    , ("fromPosix", builtin1M fromPosix)
     ]
 
 format :: Object -> Object -> Owk Object
 format (String f) (HaskellData a)
     | Just u <- cast a = return $ String $ T.pack $ formatTime defaultTimeLocale (T.unpack f) (u :: UTCTime)
+format s n@(Number _) = format s =<< fromPosix n
 format _ _ = exception $ String "time.format: type mismatch"
 
 formatLocal :: Object -> Object -> Owk Object
@@ -31,6 +34,7 @@ formatLocal (String f) (HaskellData a)
     | Just u <- cast a = do
         z <- liftIO $ utcToLocalZonedTime u
         return $ String $ T.pack $ formatTime defaultTimeLocale (T.unpack f) z
+formatLocal s n@(Number _) = formatLocal s =<< fromPosix n
 formatLocal _ _ = exception $ String "time.formatLocal: type mismatch"
 
 parse :: Object -> Object -> Owk Object
@@ -39,3 +43,7 @@ parse (String f) (String t) =
         Just u  -> return $ HaskellData (u :: UTCTime)
         Nothing -> return $ Undef
 parse _ _ = exception $ String "time.parse: type mismatch"
+
+fromPosix :: Object -> Owk Object
+fromPosix (Number s) = return $ HaskellData $ posixSecondsToUTCTime $ realToFrac s
+fromPosix _ = exception $ String "time.fromPosix: type mismatch"
